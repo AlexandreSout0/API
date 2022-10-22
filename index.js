c013e4eafb8da577f661a3ca96d8d73af74c4853
HIDROXIDO <soutoinf@gmail.com>
2022-10-22 18:12:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,19 @@
  app.use(express.json())
  app.use(cors())
   
+ let db = [
+    {'1': {Nome: 'Cliente 1', Idade: '20'} },
+    {'2': {Nome: 'Cliente 2', Idade: '21'} },
+    {'3': {Nome: 'Cliente 3', Idade: '42'} },
+    {'4': {Nome: 'Cliente 4', Idade: '25'} },
+ ]
+
+ 
+app.get('/', (req, res) => {
+
+    return res.json(db)
+    
+})
 
  app.listen(21262, () => {
     console.log('express started at http://localhost:21262')
