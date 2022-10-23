c9ddf5c28a13af1fea18e7a4a5a61d79f22d564d
HIDROXIDO <soutoinf@gmail.com>
2022-10-23 09:36:59 -0300

    ok

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  const morgan = require ('morgan')
  const cors = require('cors')
  const bodyParser = require('body-parser')
+ const routes = require('./config/routes')
+
 
  const app = express()
 
@@ -9,22 +11,11 @@
  app.use(bodyParser.urlencoded({extended: false}))
  app.use(express.json())
  app.use(cors())
+ app.use(routes)
   
- let db = [
-    {'1': {Nome: 'Cliente 1', Idade: '20'} },
-    {'2': {Nome: 'Cliente 2', Idade: '21'} },
-    {'3': {Nome: 'Cliente 3', Idade: '42'} },
-    {'4': {Nome: 'Cliente 4', Idade: '25'} },
- ]
-
  
-app.get('/', (req, res) => {
-
-    return res.json(db)
-    
-})
-
+  
  app.listen(21262, () => {
-    console.log('express started at http://localhost:21262')
+    console.log('Express started at http://localhost:21262')
 
  })
